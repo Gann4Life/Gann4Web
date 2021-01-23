f41b764f5783061316f43ea94abf7b8f9f741633
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-01-22 22:56:00 -0300

    Added and modified files.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <?php
+        echo "<p>Welcome.</p>";
+    ?>
+
+    <p>Site list:</p>
+    <ul>
+        <li>
+            <a href="thirdym.html">Thirdym outdated site</a>
+        </li>
+    </ul>
+
+    <p>This website is currently in a WIP (Work in progress) status.</p>
+</body>
+</html>
