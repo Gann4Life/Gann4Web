e50a6fe2b008974424ee6614ea2a19828ba55c39
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-01-22 23:48:08 -0300

    Im not sure what im doing, better organization is required.

---
+++ digitalart.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gann4Art</title>
+</head>
+<body>
+    <h1>Gann4Art</h1>
+    <p>Here i'm supposed to share my art.</p>
+    <a href="index.php">Go back to home</a>
+
+    <!-- This file is supposed to use the 'digitalart' folder, for loading pictures, descriptions, or whatever -->
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,20 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Gann4Life: Home</title>
 </head>
 <body>
-    <?php
-        echo "<p>Welcome.</p>";
-    ?>
+    <h1>Welcome.</h1>
 
     <p>Site list:</p>
     <ul>
         <li>
-            <a href="thirdym.html">Thirdym outdated site</a>
+            <a href="projects/thirdym/thirdym.php">Thirdym outdated site</a>
+        </li>
+        <li>
+            <a href="digitalart.php">Art page</a>
         </li>
     </ul>
 
     <p>This website is currently in a WIP (Work in progress) status.</p>
+
+    <p>I have some data around too...</p>
+    <?php 
+        echo file_get_contents("data.json");
+    ?>
 </body>
 </html>
