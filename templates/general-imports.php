e54a177371f98b370d92279ae2aa0d5242d59526
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-01-24 05:36:33 -0300

    Fixes

--- templates/general-imports.php
+++ templates/general-imports.php
@@ -1,4 +1,4 @@
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="css/style.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
 <?php echo "<title>$title</title>"; ?>
