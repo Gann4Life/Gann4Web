aaee71c74ee1ec23a177421b6865c6930e30a855
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-01-30 14:50:26 -0300

    Update contribute.php

--- sites/contribute.php
+++ sites/contribute.php
@@ -13,8 +13,10 @@
         <p>If you want to contribute to my projects, you have different options to do that.</p>
         <p><b>Support with Projecs:</b> You can provide help and suggestions to the source code of my public projects.</p>
         <p><b>Support with PayPal:</b> If you can't help with the source code, you can support me by providing a small donation. :)</p>
+        <p><b>Support with Art</b>: You can buy art i've made, in case you like what i do or want to get something in exchange of USD c:</p>
         <a href="https://github.com/gann4" class="nav-button">Support with Projects</a>
         <a href="https://www.paypal.com/donate?hosted_button_id=S59T3R6LTCEB4" class="nav-button">Support with PayPal</a>
+        <a href="https://www.deviantart.com/gann4life/shop" class="nav-button">Support with Art</a>
     </div>
     <?php include "templates/footer.php" ?>
 </body>
