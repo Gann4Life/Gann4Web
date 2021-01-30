d0cd534702421c6739a9a175c766e48f7484846f
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-01-30 15:02:06 -0300

    Update projects.php

--- sites/projects.php
+++ sites/projects.php
@@ -11,8 +11,12 @@
     <div class="content centered">
         <h1 class="content-title">Projects</h1>
         <p>In this section i'll be sharing projects in which i've been working / i am working on.</p>
-        <p>You can contribute to some of them on my github page, <a href="social-media.php">find it here</a></p>
+        <p>You can contribute to some of them on my <a href="https://github.com/gann4">github page</a>.</p>
         <a href="thirdym.php" class="nav-button">Thirdym</a>
+        <a href="https://github.com/Gann4/Gann4Web" class="nav-button">Gann4Web</a>
+        <a href="https://github.com/Gann4/Store-Manager" class="nav-button">LDP Store Manager</a>
+        <a href="https://gann4life.gamejolt.io/distractions/" class="nav-button">Distractions</a>
+        <a href="https://gann4life.itch.io/tanks-and-planes" class="nav-button">Tanks & Planes</a>
     </div>
     <?php include "templates/footer.php" ?>
 </body>
