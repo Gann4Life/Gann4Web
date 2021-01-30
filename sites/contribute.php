d2bd94b7c922f100a9e52dd6e0d405f222f067bb
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-01-30 14:47:07 -0300

    Update contribute.php

--- sites/contribute.php
+++ sites/contribute.php
@@ -13,10 +13,8 @@
         <p>If you want to contribute to my projects, you have different options to do that.</p>
         <p><b>Support with Projecs:</b> You can provide help and suggestions to the source code of my public projects.</p>
         <p><b>Support with PayPal:</b> If you can't help with the source code, you can support me by providing a small donation. :)</p>
-        <p><b>Support with Itch.io:</b> My games published on itch.io are free, however you are able to support me trough this website too!</p>
         <a href="https://github.com/gann4" class="nav-button">Support with Projects</a>
         <a href="https://www.paypal.com/donate?hosted_button_id=S59T3R6LTCEB4" class="nav-button">Support with PayPal</a>
-        <a href="https://gann4life.itch.io" class="nav-button">Support with Itch.io</a>
     </div>
     <?php include "templates/footer.php" ?>
 </body>
