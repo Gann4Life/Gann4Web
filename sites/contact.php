2bd6dcd715a0cdd1c5d2691e23ceae03369facc0
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-01-31 22:19:03 -0300

    Small changes before continuing with the contact site

--- sites/contact.php
+++ sites/contact.php
@@ -3,19 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php $cssfolder="../css"; include "templates/general-imports.php" ?>
+    <?php include "templates/general-imports.php" ?>
     <title>Contact</title>
 </head>
 <body>
-    <?php 
-        include "templates/header.php"; 
-    ?>
-    <?php 
-        $title="Contact"; 
-        include "templates/wip.php";
-    ?>
-    <?php 
-        include "templates/footer.php"; 
-    ?>
+    <?php include "templates/header.php"; ?>
+    <?php $title="Contact"; include "templates/wip.php" ?>
+    <?php include "templates/footer.php" ?>
 </body>
 </html>
