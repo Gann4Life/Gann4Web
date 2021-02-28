40b33f39248a01a970477a936b1d7cc8f07c1a5a
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-02-28 15:25:31 -0300

    Create toribash.php

---
+++ sites/toribash.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "templates/general-imports.php" ?>
+    <title>Toribash art</title>
+</head>
+<body>
+    <?php include "templates/header.php" ?>
+
+    <div class="content centered">
+        <h1 class="content-title">Toribash Stuff</h1>
+        <a href="https://www.deviantart.com/gann4life/gallery/76862697/toribash"><h1>Full DeviantArt gallery</h1></a>
+        <hr>
+        <div class="art-content">
+            <a href="https://www.deviantart.com/gann4life/art/Smiley-bundle-Toribash-head-texture-868445461" alt="">
+                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a69cd076-4094-4504-aa20-cf2c5e9226b7/ded1st1-129c35b8-002e-4035-9888-d2a339a5618c.png/v1/fill/w_1235,h_647,strp/smiley_bundle___toribash_head_texture_by_gann4life_ded1st1-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMDA2IiwicGF0aCI6IlwvZlwvYTY5Y2QwNzYtNDA5NC00NTA0LWFhMjAtY2YyYzVlOTIyNmI3XC9kZWQxc3QxLTEyOWMzNWI4LTAwMmUtNDAzNS05ODg4LWQyYTMzOWE1NjE4Yy5wbmciLCJ3aWR0aCI6Ijw9MTkyMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.ZXpjFc49iPU4WW1VyEugqpkpkV6-AbkXL-qHH_kqQ6M" alt="">
+                <h1>Smiley bundle</h1>
+            </a>
+            <a href="https://www.deviantart.com/gann4life/art/Glowing-ninja-mask-Toribash-head-texture-868449697">
+                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a69cd076-4094-4504-aa20-cf2c5e9226b7/ded1w2p-3924af9f-6d44-4e4c-9073-1b06219fa542.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTY5Y2QwNzYtNDA5NC00NTA0LWFhMjAtY2YyYzVlOTIyNmI3XC9kZWQxdzJwLTM5MjRhZjlmLTZkNDQtNGU0Yy05MDczLTFiMDYyMTlmYTU0Mi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.tIXfI0tLy9MmSUrHU1lNTq8G1K8d9cxCmLcZtdlWSyg" alt="">
+                <h1>Glowing Ninja Mask</h1>
+            </a>
+            <a href="https://www.deviantart.com/gann4life/art/Anime-like-Toribash-head-texture-868435868">
+                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a69cd076-4094-4504-aa20-cf2c5e9226b7/ded1lek-9b00960f-bbe6-4ed6-963f-5624937874a7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTY5Y2QwNzYtNDA5NC00NTA0LWFhMjAtY2YyYzVlOTIyNmI3XC9kZWQxbGVrLTliMDA5NjBmLWJiZTYtNGVkNi05NjNmLTU2MjQ5Mzc4NzRhNy5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.hKYY9TfUNME5Lu_sHXFcSpKX_Ihejm7NxLzVBsPJCtw" alt="">
+                <h1>Anime-like head</h1>
+            </a>
+            <a href="https://www.deviantart.com/gann4life/art/Ninja-mask-Toribash-head-texture-868434085">
+                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a69cd076-4094-4504-aa20-cf2c5e9226b7/ded1k11-4b41cb4e-b31a-4cf8-a312-0e3fd228e9ac.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTY5Y2QwNzYtNDA5NC00NTA0LWFhMjAtY2YyYzVlOTIyNmI3XC9kZWQxazExLTRiNDFjYjRlLWIzMWEtNGNmOC1hMzEyLTBlM2ZkMjI4ZTlhYy5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.sZZ1B-YXKiW5v13ifMK1SfJxFb8C-puziNMnlE7Aodo" alt="">
+                <h1>Ninja Mask</h1>
+            </a>
+        </div>
+    </div>
+
+    <?php include "templates/footer.php" ?>
+</body>
+</html>
