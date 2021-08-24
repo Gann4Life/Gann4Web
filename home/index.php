a38697e972e280ec166944e29ec2415e7b71cf00
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-08-24 03:11:43 -0300

    Add bootstrap elements
    Edit header.php
    Edit home.php
    Edit footer.php
    Edit general-imports.php
    Remove responsive-header.js
    
    Rest of pages need to be edited.

--- home/index.php
+++ home/index.php
@@ -6,15 +6,32 @@
     <?php include "../templates/general-imports.php" ?>
     <title>Home</title>
 </head>
-<body>
+<body class="bg-dark text-white">
     <?php include "../templates/header.php" ?>
 
-    <div class="content centered">
-        <h1 class="content-title">Welcome! :)</h1>
-        <p>Looking for games...? Art...? Music...?</p>
-        <p>You're in the right place!</p>
-        <br>
-        <a href="../about" class="nav-button">Know about me!</a>
+    <div class="container-fluid">
+        <div class="container">
+            <h1 class="text-center">Welcome to Gann4Life.ga!</h1>
+            
+            <div class="d-flex justify-content-center">
+                <button type="button" class="btn btn-light" href="../about">Know about me!</button>
+            </div>
+
+            <h2>Explore</h2>
+            <div class="border-bottom card bg-dark" style="width: 18rem;">
+                <a href="../thirdym">
+                    <img src="https://m.gjcdn.net/game-thumbnail/500/261475-2fu4affy-v4.webp" class="card-img-top" alt="...">
+                </a>
+                <div class="card-body">
+                    <h5 class="card-title">Thirdym</h5>
+                    <p class="card-text">Thirdym is a third person shooter game based on Eric Gurt's Plazma Burst series.</p>
+                </div>
+                <div class="card-body">
+                    <button type="button" class="btn btn-light card-link" href="https://gamejolt.com/games/thirdym/261475">GameJolt</button>
+                    <button type="button" class="btn btn-light card-link" href="../thirdym/download">Download</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     <?php include "../templates/footer.php" ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,6 @@
-<footer>
-    <a href="..\contribute" class="nav-button">Contribute</a>
-    <p>Created by Gann4Life</p>
+<footer class="border-top fixed-bottom navbar navbar-dark bg-dark">
+    <div class="container">
+        <a type="button" class="btn btn-dark nav-item" href="../contribute">Contribute</a>
+        <p class="text-muted">Created by Gann4Life</p>
+    </div>
 </footer>
