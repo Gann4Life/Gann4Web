221dbbdf88ad497b773e23905715e44ff7f047f6
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-08-25 00:02:45 -0300

    Update home

--- home/index.php
+++ home/index.php
@@ -14,20 +14,45 @@
             <h1 class="my-5 mx-5 text-center">Welcome to Gann4Life.ga!</h1>
             
 
-            <h2>Explore</h2>
-            <div class="border-bottom card bg-dark" style="width: 18rem;">
-                <a href="../thirdym">
-                    <img src="https://m.gjcdn.net/game-thumbnail/500/261475-2fu4affy-v4.webp" class="card-img-top" alt="...">
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title">Thirdym</h5>
-                    <p class="card-text">Thirdym is a third person shooter game based on Eric Gurt's Plazma Burst series.</p>
+            <h2 class="text-muted text-center">Explore</h2>
+            <hr>
+            <div class="justify-content-center row row-cols-4">
+                <!-- Gann4Art community -->
+                <div class="col">
+                    <div class="shadow bg-dark border-0 card" style="width: 18rem;">
+                        <img src="../img/g4abgfill.png" class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Gann4Art Community</h5>
+                            <p class="card-text">Do you have content to share? Come and show us your best works!</p>
+                            <a href="../discord" class="btn btn-primary">Join</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-light card-link" href="https://gamejolt.com/games/thirdym/261475">GameJolt</button>
-                    <button type="button" class="btn btn-light card-link" href="../thirdym/download">Download</button>
+                <!-- Thirdym -->
+                <div class="col">
+                    <div class="shadow bg-dark border-0 card" style="width: 18rem;">
+                        <img src="https://m.gjcdn.net/game-screenshot/300/8301331-s6iaph3g-v4.webp" class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Thirdym</h5>
+                            <p class="card-text">Thirdym is a third person shooter game with active ragdoll physics based on Eric Gurt's Plazma Burst series.</p>
+                            <a href="https://gamejolt.com/games/thirdym/261475" class="btn btn-primary">Page</a>
+                            <a href="https://link-to.net/172700/thirdym" class="btn btn-primary">Download</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Toribash art -->
+                <div class="col">
+                    <div class="shadow bg-dark border-0 card" style="width: 18rem;">
+                        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a69cd076-4094-4504-aa20-cf2c5e9226b7/ded1k11-3122103e-962f-4eb8-8a18-5b486bf5cde6.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E2OWNkMDc2LTQwOTQtNDUwNC1hYTIwLWNmMmM1ZTkyMjZiN1wvZGVkMWsxMS0zMTIyMTAzZS05NjJmLTRlYjgtOGExOC01YjQ4NmJmNWNkZTYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Ex5Gt03K0HjrnkZFPVFlgRmfmlQyUaND4sLCZZyZLJw" class="card-img-top" alt="...">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Toribash art</h5>
+                            <p class="card-text">I make toribash textures, only TC or USD commisions.</p>
+                            <a href="../toribash" class="btn btn-primary">See content</a>
+                        </div>
+                    </div>    
                 </div>
             </div>
+
         </div>
     </div>
 
