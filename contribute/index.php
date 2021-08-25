1be36dcfec6959608f2285099cd6dc614e42f0c0
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-08-24 22:42:22 -0300

    Update contribute & donation-success pages

--- contribute/index.php
+++ contribute/index.php
@@ -12,14 +12,58 @@
 
         <div class="container">
             <h1 class="my-5 text-center">Contribute</h1>
-            <p>If you want to contribute to my projects, you have different options to do that.</p>
-            <p><b>Support with Projecs:</b> You can provide help and suggestions to the source code of my public projects.</p>
-            <p><b>Support with PayPal:</b> If you can't help with the source code, you can support me by providing a small donation. :)</p>
-            <p><b>Support with Art</b>: You can buy art i've made, in case you like what i do or want to get something in exchange of USD c:</p>
 
-            <button type="button" href="https://github.com/gann4" class="btn btn-primary nav-button">Support with Projects</button>
-            <button type="button" href="https://www.paypal.com/donate?hosted_button_id=S59T3R6LTCEB4" class="btn btn-primary nav-button">Support with PayPal</button>
-            <button type="button" href="https://www.deviantart.com/gann4life/shop" class="btn btn-primary nav-button">Support with Art</button>
+            <p class="text-center">You have different ways in which you can contribute to my content...</p>
+
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-4">
+                    <div class="shadow border-0 my-2 bg-dark card">
+                        <div class="card-body border-bottom">
+                            <h5 class="card-title">Contribute by building along me</h5>
+                            <p class="card-text">You can provide help and suggestions to the source code of my public projects.</p>
+                            <a type="button" href="https://github.com/gann4" class="btn btn-primary nav-button">Contribute</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="shadow border-0 my-2 bg-dark card">
+                        <div class="card-body border-bottom">
+                            <h5 class="card-title">Donate by using PayPal</h5>
+                            <p class="card-text">You can support me trough direct donations using PayPal.</p>
+                            <a type="button" href="https://www.paypal.com/donate?hosted_button_id=S59T3R6LTCEB4" class="btn btn-primary nav-button">Donate with PayPal</a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-4">
+                    <div class="shadow border-0 my-2 bg-dark card">
+                        <div class="card-body border-bottom">
+                            <h5 class="card-title">Buy my art</h5>
+                            <p class="card-text">You can buy any art and/or prints i make from these too!</p>
+                            <a type="button" href="https://www.deviantart.com/gann4life/shop" class="btn btn-primary nav-button">Buy art</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <div class="row row-cols-3">
+                <div class="shadow col">
+                    <p>You can provide help and suggestions to the source code of my public projects.</p>
+                    <a type="button" href="https://github.com/gann4" class="btn btn-primary nav-button">Help to develop projects</a>
+                </div>
+                <div class="shadow border-start border-end col">
+                    <p>You can support me trough direct donations.</p>
+                    <a type="button" href="https://www.paypal.com/donate?hosted_button_id=S59T3R6LTCEB4" class="btn btn-primary nav-button">Donate with PayPal</a>
+                </div>
+                <div class="shadow col">
+                    <p>You can buy art i made too!</p>
+                    <a type="button" href="https://www.deviantart.com/gann4life/shop" class="btn btn-primary nav-button">Buy art</a>
+                </div>
+            </div> -->
+
+
+
         </div>
     </div>
     <?php include "../templates/footer.php" ?>
--- donation-success/index.php
+++ donation-success/index.php
@@ -3,19 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thank you!</title>
+    <title>Donation success!</title>
     <?php include "../templates/general-imports.php" ?>
 </head>
-<body>
+<body class="bg-dark text-white">
     <?php include "../templates/header.php" ?>
-    <div class="content centered">
-        <h1 class="content-title">Thank you!</h1>
-        <p>You've successfully made your donation! So unexpected :0!</p>
-        <p>Thank you very much for this very kind action, i really appreciate it. <3</p>
-        <br>
-        <p>I can't do anything special for you at the moment, however if you save the transaction ID, maybe i can do something for you later :)</p>
-        <p>Have a great day/afternoon/night. \nwn/</p>
+
+    <div class="container-fluid">
+        <div class="container text-center">
+            <h1 class="my-5">♥ Thank you! ♥</h1>
+            <p>You've successfully made your donation! That was unexpected :o <br>
+                Thank you very much for this very kind action, i really appreciate it. ♥
+            </p>
+            <br>
+            <p>I can't do anything special for you at the moment except for increasing my content creation speed. 
+                <br> However, if you save the transaction ID, maybe i can do something for you in the future :)</p>
+            <p>Have a great day/afternoon/night. \nwn/</p>
+        </div>
     </div>
+
     <?php include "../templates/footer.php" ?>
 </body>
 </html>
