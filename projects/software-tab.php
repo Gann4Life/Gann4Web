3c53a5bdbc9eaf64a778999120e8ba856ead0de3
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-08-24 22:18:20 -0300

    Update software-tab.php

--- projects/software-tab.php
+++ projects/software-tab.php
@@ -10,8 +10,8 @@
                 <img src="https://m.gjcdn.net/game-thumbnail/500/261475-2fu4affy-v4.webp" class="card-img-top" alt="...">
                 </a> -->
                 <div class="card-body">
-                    <h5 class="card-title">Gann4Web</h5>
-                    <p class="card-text">You are currently navigating trough Gann4Web. :)</p>
+                    <h5 class="card-title">MediafireDL</h5>
+                    <p class="card-text">MediafireDL is an automation tool to download files from mediafire without having to do any extra steps.</p>
                 </div>
                 <div class="card-body bg-dark">
                     <a type="button" class="btn btn-light card-link" href="https://github.com/Gann4/mediafiredl">GitHub</a>
