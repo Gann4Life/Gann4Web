a8e3391a3c05342d25968467943aa0fa2d39e98f
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-10-11 06:06:36 -0300

    Add linkvertise affiliate ads

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,7 @@
+<div class="container text-center">
+    <div style="position: relative;"><iframe src="https://publisher.linkvertise.com/cdn/ads/LV-728x90/index.html" frameborder="0" height="90" width="728"></iframe><a href="https://publisher.linkvertise.com/ac/172700" target="_blank" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;"></a></div>
+</div>
+
 <hr class="my-5">
 
 <footer class="shadow border-top fixed-bottom navbar navbar-dark bg-dark">
